525d8b5f784d8e3aeab5e81254c7af75e07b75eb
Damian <damiankontakt7@gmail.com>
2026-04-20 21:13:45 +0200

    implementacja ArtifactRepository

---
+++ src/repositories/ArtifactRepository.ts
@@ -0,0 +1,16 @@
+﻿import { ArtifactModel, IArtifact } from '../models/Artifact';
+
+export class ArtifactRepository {
+    async findAll(): Promise<IArtifact[]> {
+        return await ArtifactModel.find().populate('owner');
+    }
+
+    async create(data: Partial<IArtifact>): Promise<IArtifact> {
+        const artifact = new ArtifactModel(data);
+        return await artifact.save();
+    }
+
+    async deleteByOwnerId(ownerId: string): Promise<void> {
+        await ArtifactModel.deleteMany({ owner: ownerId });
+    }
+}
