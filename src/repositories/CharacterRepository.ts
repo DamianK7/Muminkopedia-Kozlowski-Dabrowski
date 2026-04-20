64c8fc6608281910f45f631c082622889e1a62a8
Damian <damiankontakt7@gmail.com>
2026-04-20 21:12:53 +0200

    implementacja CharacterRepository

---
+++ src/repositories/CharacterRepository.ts
@@ -0,0 +1,20 @@
+﻿import { CharacterModel, ICharacter } from '../models/Character';
+
+export class CharacterRepository {
+    async findAll(): Promise<ICharacter[]> {
+        return await CharacterModel.find().populate('bestFriend');
+    }
+
+    async findById(id: string): Promise<ICharacter | null> {
+        return await CharacterModel.findById(id).populate('bestFriend');
+    }
+
+    async create(data: Partial<ICharacter>): Promise<ICharacter> {
+        const character = new CharacterModel(data);
+        return await character.save();
+    }
+
+    async delete(id: string): Promise<ICharacter | null> {
+        return await CharacterModel.findByIdAndDelete(id);
+    }
+}
