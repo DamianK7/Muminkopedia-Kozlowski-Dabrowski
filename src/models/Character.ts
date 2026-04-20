e318ccd91689c54ff30932de1be1aeaaaa8cd7e9
Damian <damiankontakt7@gmail.com>
2026-04-20 20:07:10 +0200

    utworzenie modelu postaci

---
+++ src/models/Character.ts
@@ -0,0 +1,19 @@
+﻿import mongoose, { Schema, Document, Types } from 'mongoose';
+
+export interface ICharacter extends Document {
+    name: string;
+    description?: string;
+    species: string;
+    isHibernating: boolean;
+    bestFriend?: Types.ObjectId;
+}
+
+const CharacterSchema: Schema = new Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    species: { type: String, required: true },
+    isHibernating: { type: Boolean, default: false },
+    bestFriend: { type: Schema.Types.ObjectId, ref: 'Character' }
+}, { timestamps: true });
+
+export const CharacterModel = mongoose.model<ICharacter>('Character', CharacterSchema);
